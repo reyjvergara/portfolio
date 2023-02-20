3a45ce088aa1968e5ca459e848333893157be632
reyjvergara <rey.j.vergara@gmail.com>
2023-02-19 23:05:56 -0800

    Updated angular index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['../demo-styling.css', '../app/app.component.css']
 })
 export class AppComponent {
   title = 'portfolio';
